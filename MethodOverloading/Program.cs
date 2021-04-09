18d009256a88e97b8816e5a8e7d6317ade331dcd
David Harris <davlsu14@gmail.com>
2021-04-09 13:35:52 -0500

    completed exercise

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static int ADD(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal ADD(decimal z, decimal w)
+        {
+            return z + w;
+        }
+
+        public static string ADD( int x ,int y, bool money)
+        {
+            if  (money == true)
+
+            {
+                if( x + y == 1)
+                {
+                    return $" {x + y}: dollar";
+                }
+                return $" {x + y}: dollars"; 
+                
+            }
+            else
+            {
+                return $"error";
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            bool money = true;
+            Console.WriteLine(ADD(7,6, money));
+        }
+    }
+}
